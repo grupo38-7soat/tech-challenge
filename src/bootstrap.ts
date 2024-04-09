87ed09d888f63fd7d93eb8af143ad06905a9dfd8
Lucas Santos <lucasa95santos@gmail.com>
2024-04-08 23:45:09 -0300

    chore: adicionando o express no projeto

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,1 +1,16 @@
-console.log('Hello world')
+import express from 'express'
+
+const SERVER_PORT = 3000
+const app = express()
+app.use(express.json())
+app.get('', (request, response) => {
+  console.log('Rota: ', {
+    url: request.url,
+    method: request.method,
+    body: request.body,
+  })
+  response.send('OK')
+})
+app.listen(SERVER_PORT, () => {
+  console.log(`Server running on port ${SERVER_PORT}`)
+})
