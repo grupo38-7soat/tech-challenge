758df7a30f0e68f3bc5e3155f7a05ebad571cc1e
Lucas Santos <lucasa95santos@gmail.com>
2024-05-22 08:17:02 -0300

    chore: editando a tabela cliente

--- database/01_create_database.sql
+++ database/01_create_database.sql
@@ -12,28 +12,26 @@ end if;
 end
 $$;
 
-create table if not exists fast_food.client (
-    id bigserial,
+create table if not exists fast_food.customer (
+    id uuid,
     name varchar(255) not null,
-    address varchar(255) not null,
-    cell_phone varchar(255) not null,
-    document varchar(255) not null,
-    zip_code char(8) not null,
-    create_at timestamp default current_timestamp,
-    update_at timestamp default current_timestamp,
-    constraint pk_client primary key (id)
+    document varchar(11) not null unique,
+    email varchar(255) not null,
+    created_at timestamp default current_timestamp,
+    updated_at timestamp default current_timestamp,
+    constraint pk_customer primary key (id)
 );
 
 create table if not exists fast_food.order (
     id bigserial,
-    client_id integer,
+    customer_id uuid,
     order_date timestamp default current_timestamp,
     amount numeric(10, 2) not null,
     status varchar(255) not null default 'Pedido Recebido',
-    create_at timestamp default current_timestamp,
-    update_at timestamp default current_timestamp,
+    created_at timestamp default current_timestamp,
+    updated_at timestamp default current_timestamp,
     constraint pk_order primary key (id),
-    constraint fk_order_client foreign key (client_id) references fast_food.client(id)
+    constraint fk_order_customer foreign key (customer_id) references fast_food.customer(id)
 );
 
 create table if not exists fast_food.product (
@@ -43,8 +41,8 @@ create table if not exists fast_food.product (
     category varchar(255) not null,
     price decimal(10, 2) not null,
     link_photo text,
-    create_at timestamp default current_timestamp,
-    update_at timestamp default current_timestamp,
+    created_at timestamp default current_timestamp,
+    updated_at timestamp default current_timestamp,
     constraint pk_product primary key (id)
 );
 
@@ -55,8 +53,8 @@ create table if not exists fast_food.product_order (
     quantity integer not null,
     unit_price decimal(10, 2) not null,
     observation text,
-    create_at timestamp default current_timestamp,
-    update_at timestamp default current_timestamp,
+    created_at timestamp default current_timestamp,
+    updated_at timestamp default current_timestamp,
     constraint pk_product_order primary key (id),
     constraint fk_product_order_product foreign key (product_id) references fast_food.product(id),
     constraint fk_product_order_order foreign key (order_id) references fast_food.order(id)
