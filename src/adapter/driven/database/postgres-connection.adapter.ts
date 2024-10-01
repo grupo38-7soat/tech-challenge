09d96e96f2b651b95bf4fd8f987ed59408219127
Lucas Santos <lucasa95santos@gmail.com>
2024-09-30 21:20:37 -0300

    refactor: improve postgreSQL connection

--- src/adapter/driven/database/postgres-connection.adapter.ts
+++ src/adapter/driven/database/postgres-connection.adapter.ts
@@ -1,46 +1,23 @@
 import { globalEnvs } from '@config/envs/global'
-import { Client, QueryResult } from 'pg'
+import { Pool, QueryResult } from 'pg'
 
 export class PostgresConnectionAdapter {
-  private client: Client
+  private pool: Pool
 
   constructor() {
-    this.client = null
-  }
-
-  private async connect(): Promise<void> {
-    if (!this.client) {
-      this.client = new Client({
-        host: globalEnvs.database.host,
-        port: globalEnvs.database.port,
-        database: globalEnvs.database.name,
-        user: globalEnvs.database.user,
-        password: globalEnvs.database.password,
-      })
-      await this.client.connect()
-    }
-  }
-
-  private async destroy(): Promise<void> {
-    if (this.client) {
-      await this.client.end()
-      this.client = null
-    }
+    this.pool = null
   }
 
   async query<T>(sqlQuery: string, params: unknown[]): Promise<QueryResult<T>> {
     await this.connect()
     console.log(`[Database] ${sqlQuery}`)
-    const result = await this.client.query(sqlQuery, params)
-    await this.destroy()
+    const result = await this.pool.query(sqlQuery, params)
     return result
   }
 
   async checkDatabase(): Promise<boolean> {
     try {
-      await this.connect()
       const result = await this.query('SELECT 1', [])
-      await this.destroy()
       if (result.rowCount === 1) {
         return true
       }
@@ -48,6 +25,25 @@ export class PostgresConnectionAdapter {
     } catch (error) {
       console.error(`[Database] ${error.message}`)
       return false
+    }
+  }
+
+  private async connect(): Promise<void> {
+    if (!this.pool) {
+      console.log('[Database] Abrindo conexão com a base de dados')
+      this.pool = new Pool({
+        host: globalEnvs.database.host,
+        port: globalEnvs.database.port,
+        database: globalEnvs.database.name,
+        user: globalEnvs.database.user,
+        password: globalEnvs.database.password,
+      })
+      process.on('exit', () => {
+        this.pool.end(() => {
+          console.log('[Database] Fechando conexão com a base de dados')
+        })
+        this.pool = null
+      })
     }
   }
 }
